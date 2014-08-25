9341c57cb1c6b1badaa1a3c7f5a4e441ec6499d3
amandascchan <amanda.chan@metricwire.com>
2014-08-25 00:44:33 -0400

    image scrolling with images

--- public/controllers/study.client.controller.js
+++ public/controllers/study.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var myApp = angular.module('Study', []);
+var myApp = angular.module('Study', ['underscore']);
 
 myApp.controller('StudyController', ['$scope', function($scope) {
     $scope.hello = 'helleeeee';
@@ -14,15 +14,44 @@ function Main($scope) {
     $scope.loadMore = function() {
         for (var i = 0; i < 10; i++) {
             $scope.items.push({id: counter});
-            counter += 10;
+            counter += 5;
         }
-    };
-    $scope.loadImages = function() {
-
-    };
-     
+    };     
     $scope.loadMore();
 }
+
+function UserCtrl($scope, $http) {
+    $scope.phoneText = [];
+    $scope.phoneImages = [];
+    $scope.imageText = {};
+    $http.get('/listOfAllPhones.txt').success(function(data, status, headers, config) {
+        if (data && status === 200) {
+            $scope.phoneText = data.split('\n');
+        }
+    });
+    $http.get('/phoneImages.txt').success(function(data, status, headers, config) {
+        if(data && status == 200) {
+            $scope.phoneImages = data.split('\n');
+            $scope.updatePhoneValues();
+        }
+    });
+    $scope.updatePhoneValues = function () {
+        if($scope.phoneText.length > 0 && $scope.phoneImages.length > 0) {
+            for(var i = 0; i < $scope.phoneImages.length; i++) {
+                var phoneStr = $scope.phoneImages[i];
+                phoneStr = phoneStr.substring(0, phoneStr.length - 4);
+                phoneStr = phoneStr.replace(/_/g, ' ');
+                if(_.indexOf($scope.phoneText, phoneStr) === -1) {
+                    $scope.phoneImages.splice(i, 1);
+                }
+                else {
+                    $scope.imageText = $scope.imageText || {};
+                    $scope.imageText[phoneStr] = $scope.phoneImages[i];
+                }
+            }
+        }
+    }
+};
 
 
 myApp.directive('whenScrolled', function() {
@@ -36,3 +65,4 @@ myApp.directive('whenScrolled', function() {
         });
     };
 });
+
--- web.js
+++ web.js
@@ -1,7 +1,12 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var path = require("path");
+var http = require('http'),
+    path = require('path'),
+    fs = require('fs'),
+    util = require('util'),
+    express = require('express'),
+    logfmt = require("logfmt");
+
 var app = express();
+
 
 app.set('views', __dirname + '/public/views/');
 app.engine('html', require('ejs').renderFile);
@@ -11,6 +16,27 @@ app.use(logfmt.requestLogger());
 app.get('/', function(req, res) {
     res.render("test.html");
 });
+
+var filePath = path.join(__dirname, 'public/views/listOfAllPhones.txt');
+app.get('/listOfAllPhones.txt' , function(req , res){ 
+	res.setHeader('Content-type' , 'text/plain'); 
+	fs.readFile(filePath , function(err , data){ 
+		res.end(data); 
+	}); 
+});
+var filePath2 = path.join(__dirname, 'public/views/phoneImages/phoneImages.txt');
+app.get('/phoneImages.txt' , function(req , res){ 
+	res.setHeader('Content-type' , 'text/plain'); 
+	fs.readFile(filePath2, function(err , data){ 
+		res.end(data); 
+	}); 
+});
+
+app.get('/phoneImages/:file', function(req, res){
+	var file = req.params.file;
+	var filePath3 = path.join(__dirname, 'public/views/phoneImages/'+file);
+	res.sendfile(filePath3);
+})
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
